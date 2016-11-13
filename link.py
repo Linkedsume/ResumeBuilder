5fa4f52741fd4afc54d1ee217d876b5353633521
Jonathan Stein <jonathanstein98@gmail.com>
2016-11-13 11:13:14 -0800

    made json fit schema for json resume

--- link.py
+++ link.py
@@ -1,5 +1,6 @@
 from requests_oauthlib import OAuth2Session
 from requests_oauthlib.compliance_fixes import linkedin_compliance_fix
+import json
 
 # from linkedin.linkedin import (LinkedInAuthentication, LinkedInApplication, PERMISSIONS)
 
@@ -17,5 +18,43 @@ if __name__ == '__main__':
     print 'Please go here and authorize,', authorization_url
     redirect_response = raw_input('Paste the full redirect URL here:')
     linkedin.fetch_token(TOKEN_URL, client_secret=CLIENT_SECRET, authorization_response=redirect_response)
-    r = linkedin.get("https://api.linkedin.com/v1/people/~?format=json")
-    print r.content
+    # email = linkedin.get("https://api.linkedin.com/v1/people/~/email-address/?format=json
+    info = linkedin.get("https://api.linkedin.com/v1/people/~:(id,email-address,first-name,last-name,industry,positions,summary,headline,location)/?format=json")
+    infoJson = json.loads(info.content)
+    # print email.content
+    print info.content
+    data = {
+    "basics": {
+    "name": "",
+    "label": "",
+    "picture": "",
+    "email": "",
+    "phone": "",
+    "website": "",
+    "summary": "",
+    "location": {
+    "address": "",
+    "postalCode": "",
+    "city": "",
+    "countryCode": "",
+    "region": ""
+    },
+    "profiles": []
+    },
+    "work": [],
+    "volunteer": [],
+    "education": [],
+    "awards": [],
+    "publications": [],
+    "skills": [],
+    "languages": [],
+    "interests": [],
+    "references": []
+    }
+    data["basics"]["email"] = infoJson["emailAddress"]
+    data["basics"]["name"] = infoJson["firstName"] + " " + infoJson["lastName"]
+    data["basics"]["location"] = infoJson["location"]["name"]
+    data["basics"]["countryCode"] = infoJson["location"]["country"]["code"]
+    if "summary" in infoJson:
+        data["basics"]["summary"] = infoJson["summary"]
+    data["basics"]["label"] = infoJson["headline"]
